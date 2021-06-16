f90d371c9b164bdf97978ef7f47c1b9969d90e3c
Artiom Suqiasyan <artiom.suqiasyan1401@gmail.com>
2021-06-16 12:54:15 +0400

    feat: add sign in form

--- src/script.js
+++ src/script.js
@@ -32,6 +32,7 @@ const importSearch = (div) => {
 const importNav = (div) => {
   const SignIn = document.createElement("button");
   SignIn.innerText = "SignIn";
+  SignIn.onclick = SignInForm;
   const SignUp = document.createElement("button");
   SignUp.innerText = "SignUp";
   SignUp.onclick = RegisterForm;
@@ -81,4 +82,56 @@ function regInputs(div) {
   passwordLab.append(passwordInp);
   div.append(nameLab, surnameLab, emailLab, phoneLab, passwordLab);
 }
+
+let countSign = 0;
+function SignInForm() {
+  if (countSign === 0) {
+    const main = document.querySelector("main");
+    let cover = document.createElement("div");
+    cover.classList.add("cover");
+    let registerForm = document.createElement("div");
+    registerForm.classList.add("signInForm");
+    let h2 = document.createElement("h2");
+    h2.innerText = "Sign In";
+    let inputs = document.createElement("table");
+    inputs.classList.add("inputs");
+    SignInputs(inputs);
+    registerForm.append(h2, inputs);
+    cover.append(registerForm);
+    main.append(cover);
+    countSign++;
+  }
+}
+
+function SignInputs(table) {
+  let trEmail = document.createElement("tr");
+  let tdLabelEmail = document.createElement("td");
+  let emailLab = document.createElement("label");
+  emailLab.innerText = "Email*";
+  tdLabelEmail.append(emailLab);
+  let tdInputEmail = document.createElement("td");
+  let emailInp = document.createElement("input");
+  emailInp.setAttribute("placeholder", "Email")
+  tdInputEmail.append(emailInp);
+  trEmail.append(tdLabelEmail, tdInputEmail);
+
+  let trPassword = document.createElement("tr");
+  let tdLabelPassword = document.createElement("td");
+  let passwordLab = document.createElement("label");
+  passwordLab.innerText = "Password*";
+  tdLabelPassword.append(passwordLab);
+
+  let tdInputPassword = document.createElement("td");
+  let passwordInp = document.createElement("input");
+  passwordInp.setAttribute("placeholder", "Password")
+
+  tdInputPassword.append(passwordInp);
+  trPassword.append(tdLabelPassword, tdInputPassword);
+
+  let buttonSign = document.createElement("button");
+  buttonSign.innerText = "Sign In";
+  buttonSign.classList.add("signIn-button");
+  table.append(trEmail, trPassword, buttonSign);
+}
+
 run();

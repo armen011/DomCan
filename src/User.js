911ad5a4eb1d1eacdd25f704770269573f571eb4
ARMEN MKRTCHYAN <armen21mkrtchyan0616@gmail.com>
2021-06-16 10:57:39 +0400

    feat:added registerform Not complited

---
+++ src/User.js
@@ -0,0 +1,40 @@
+class User {
+  constructor(name, surname, email, phone) {
+    this.name = name;
+    this.surname = surname;
+    this.email = email;
+    this.phone = phone;
+    this.id = 1;
+    this.addUser();
+  }
+  addUser() {
+    if (localStorage.users) {
+      let users = JSON.parse(localStorage.users);
+      users.push({
+        name: this.name,
+        surname: this.surname,
+        email: this.email,
+        phone: this.phone,
+        id: users.length + 1,
+      });
+      localStorage.users = JSON.stringify(users);
+    } else {
+      let users = [
+        {
+          name: this.name,
+          surname: this.surname,
+          email: this.email,
+          phone: this.phone,
+          id: 1,
+        },
+      ];
+      localStorage.users = JSON.stringify(users);
+    }
+  }
+}
+let user = new User(
+  "Armen ",
+  "Mkrtchyan",
+  "armenmkrtchyan14@mail.ru",
+  "+37494215007"
+);
--- src/script.js
+++ src/script.js
@@ -8,6 +8,7 @@ function run() {
   importSearch(search);
   let nav = document.createElement("div");
   nav.classList.add("navbar");
+  importNav(nav);
   header.append(logo, search, nav);
 }
 const importLogo = (div) => {
@@ -18,8 +19,66 @@ const importSearch = (div) => {
   let searchcomp = document.createElement("div");
   searchcomp.classList.add("searchcomp");
   let input = document.createElement("input");
+  input.placeholder = "Write to Search";
   let btn = document.createElement("button");
+  let i = document.createElement("i");
+  i.classList.add("fa-search");
+  i.classList.add("fa");
+
+  btn.append(i);
   searchcomp.append(input, btn);
   div.append(searchcomp);
 };
+const importNav = (div) => {
+  const SignIn = document.createElement("button");
+  SignIn.innerText = "SignIn";
+  const SignUp = document.createElement("button");
+  SignUp.innerText = "SignUp";
+  SignUp.onclick = RegisterForm;
+  const Cart = document.createElement("button");
+  Cart.innerText = "Cart";
+  div.append(SignIn, SignUp, Cart);
+};
+let count = 0;
+function RegisterForm() {
+  if (count === 0) {
+    const main = document.querySelector("main");
+    let cover = document.createElement("div");
+    cover.classList.add("cover");
+    let registerForm = document.createElement("div");
+    registerForm.classList.add("registerForm");
+    let h2 = document.createElement("h2");
+    h2.innerText = "Registration";
+    let inputs = document.createElement("div");
+    inputs.classList.add("inputs");
+    regInputs(inputs);
+    registerForm.append(h2, inputs);
+    cover.append(registerForm);
+    main.append(cover);
+    count++;
+  }
+}
+function regInputs(div) {
+  let nameLab = document.createElement("label");
+  nameLab.innerText = "Name*";
+  let nameInp = document.createElement("input");
+  nameLab.append(nameInp);
+  let surnameLab = document.createElement("label");
+  surnameLab.innerText = "Surname*";
+  let surnameInp = document.createElement("input");
+  surnameLab.append(surnameInp);
+  let emailLab = document.createElement("label");
+  emailLab.innerText = "Email*";
+  let emailInp = document.createElement("input");
+  emailLab.append(emailInp);
+  let phoneLab = document.createElement("label");
+  phoneLab.innerText = "Phone*";
+  let phoneInp = document.createElement("input");
+  phoneLab.append(phoneInp);
+  let passwordLab = document.createElement("label");
+  passwordLab.innerText = "Password*";
+  let passwordInp = document.createElement("input");
+  passwordLab.append(passwordInp);
+  div.append(nameLab, surnameLab, emailLab, phoneLab, passwordLab);
+}
 run();

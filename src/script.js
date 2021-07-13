ec9b7296ca85eb46044e5399644ac82da6c7a978
Armen <armen21mkrtchyan0616@gmail.com>
2021-07-13 15:58:17 +0400

    feat:added menuicon

--- src/script.js
+++ src/script.js
@@ -11,7 +11,19 @@ function run() {
   let nav = document.createElement("div");
   nav.classList.add("navbar");
   importNav(nav);
-  header.append(logo, search, nav);
+  const menuIcon = document.createElement('div')
+  menuIcon.classList.add('container')
+  const bar1 = document.createElement('div')
+  bar1.classList.add('bar1')
+  const bar2 = document.createElement('div')
+  bar2.classList.add('bar2')
+  const bar3 = document.createElement('div')
+  bar3.classList.add('bar3')
+  const menu = document.createElement('div')
+  menu.classList.add('sideMenu')
+  importNav(menu)
+  menuIcon.append(bar1, bar2, bar3, menu)
+  header.append(logo, search, nav, menuIcon);
   let main = document.querySelector(".main");
   main.innerHTML = ''
   showItems(main);
